1b6d1d6b8431f2344872b894783aa15c80a2419d
piotr <piotr@example.com>
2026-02-05 18:35:37 +0100

    commit1

---
+++ accountTest1ZakladanieKonta.java
@@ -0,0 +1,45 @@
+package XOPERO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class accountTest1ZakladanieKonta {
+    @Test
+    public void account1() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.get("https://xopero.com/pl/");
+        String currentWindow = driver.getWindowHandle();
+        Set<String> windowsNames = driver.getWindowHandles();
+        for(String window : windowsNames){
+            if(!window.equals(currentWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+        driver.findElement(By.xpath("//*[@id='CybotCookiebotDialogBodyLevelButtonAccept']")).click();
+        driver.switchTo().window(currentWindow);
+        driver.findElement(By.xpath("/html/body/header/div[2]/nav/ul/li[2]/label/div/img")).click();
+        driver.findElement(By.xpath("/html/body/header/div[2]/nav/ul/li[2]/div/ul/li[1]/a")).click();
+        Set<String> windowsNames1 = driver.getWindowHandles();
+        for(String window : windowsNames1){
+            if(!window.equals(currentWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div/form[1]/p/a")).click();
+        driver.findElement(By.xpath("//*[@id='email1']")).sendKeys("mail@interia.pl");
+        Thread.sleep(1500);
+        Actions actions = new Actions(driver);
+        WebElement heading = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div[2]/div[1]/form/button"));
+        actions.moveToElement(heading).perform();
+      
+    }
+}
