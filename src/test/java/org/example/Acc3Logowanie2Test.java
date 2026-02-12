8c09bf0526450f2f52e32128f9164f4c350039c2
piotr <piotr@example.com>
2026-02-12 21:23:35 +0100

    commit2

---
+++ src/test/java/org/example/Acc3Logowanie2Test.java
@@ -0,0 +1,43 @@
+package XOPERO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class Acc3Logowanie2Test {
+    @Test
+    public void account3() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.get("https://xopero.com/pl/");
+        String currentWindow = driver.getWindowHandle();
+        Set<String> windowsNames = driver.getWindowHandles();
+        for(String window : windowsNames){
+            if(!window.equals(currentWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+        driver.findElement(By.xpath("//*[@id='CybotCookiebotDialogBodyLevelButtonAccept']")).click();
+        driver.switchTo().window(currentWindow);
+        driver.findElement(By.xpath("/html/body/header/div[2]/nav/ul/li[2]/label/div/img")).click();
+        driver.findElement(By.xpath("/html/body/header/div[2]/nav/ul/li[2]/div/ul/li[1]/a")).click();
+        Set<String> windowsNames1 = driver.getWindowHandles();
+        for(String window : windowsNames1){
+            if(!window.equals(currentWindow)){
+                driver.switchTo().window(window);
+            }
+        }
+        driver.findElement(By.xpath("//*[@id='loginProtect']")).sendKeys("piotrinteria.pl");
+driver.findElement(By.id("submitBtn")).click();
+Thread.sleep(1500);
+driver.quit();
+
+
+
+
+}}
